19c4d886e0424e9891a5f359468a0614427fb611
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-06-19 10:46:17 -0400

    second commit to implement first round of updates

--- index.js
+++ index.js
@@ -17,59 +17,109 @@ map = new Map(document.querySelector("#map"), {
 document.querySelector("p").addEventListener("dblclick", e =>{
   e.preventDefault();
   window.alert("Welcome to Google Mapper! Let's play around with the Google Maps API.  \n\nLook up a location by entering values for Latitude and Longitude and pressing \"Go!\".  If the spot searched is one of your favorites click \"Add to favorites\" then \"Go!\" to see your list of favorites on the right of the screen when refreshed. \n\nClick \"Get Current Location\" to see where you are.  \n\nClick \"Explore\" to go to a random place on Earth.  Values for what's found are pre-populated into the \"Latitude\" and \"Longitude\" fields to see where you are.  If you like what you found: enter a location for it, check the favorites box, and hit \"Go!\"\n\n Enjoy!")
+})
+
+///////////////////////////////////////////////////////////////////
+
+
+// document.querySelector("form").addEventListener("submit", e =>{
+//   e.preventDefault();
+//   const x = parseFloat(e.target.latitude.value);
+//   const y = parseFloat(e.target.longitude.value);
+//   const location = e.target.location.value;
+//   if(e.target["add-to-list"].checked){
+//     const newFavorite = {
+//       location: location,
+//       latitude: x,
+//       longitude: y
+//     }
+//     fetch("http://localhost:3000/favorites",{
+//       method:"POST",
+//       headers:{
+//         //sending JSON to db
+//         "Content-Type": "applications/json",
+//         //expecting a copy of the JSON back as confirmation
+//         "Accept": "applications/json" 
+//     },
+//     body: JSON.stringify(newFavorite)
+//     })
+//     .then(res => res.json())
+//     .then(newFavoriteInDB)
+//   }
+//   if(!isNaN(x) && x <= 90 && x>=-90 && y <= 180 && y>=-180 &&!isNaN(y)){
+//     const newPosition = {lat:x, lng:y}
+//     const zoom = 11
+//     map.zoom = zoom
+//     map.setCenter(newPosition)
+//   } else{
+//     console.error("Woof invalid coordinates")
+//   }
+//   });
+
+const form = document.querySelector("form")
+
+let clicked = null;
+
+document.querySelector("#look-up-value").addEventListener("click", ()=>{
+  clicked = "look-up-value";
+})
+
+document.querySelector("#add-to-favorites").addEventListener("click", () =>{
+  clicked = "add-to-favorites"
+})
+
+form.addEventListener("submit", e=>{
+  const x = parseFloat(e.target.latitude.value);
+  const y = parseFloat(e.target.longitude.value);
+  const location = e.target.location.value;
+  if(clicked ==="look-up-value"){
+    e.preventDefault();
+    if(!isNaN(x) && x <= 90 && x>=-90 && y <= 180 && y>=-180 &&!isNaN(y)){
+      const newPosition = {lat:x, lng:y}
+      const zoom = 11
+      map.zoom = zoom
+      map.setCenter(newPosition)
+    } 
+    else {
+      window.alert("Woof invalid coordinates")
+    }
+
+  } else if (clicked ==="add-to-favorites"){
+    // e.preventDefault();
+    if(location !== ""){
+      const newFavorite = {
+        location: location,
+        latitude: x,
+        longitude: y
+      }
+      fetch("http://localhost:3000/favorites",{
+        method:"POST",
+        headers:{
+          //sending JSON to db
+          "Content-Type": "application/json",
+          //expecting a copy of the JSON back as confirmation
+          "Accept": "application/json" 
+      },
+      body: JSON.stringify(newFavorite)
+      })
+      .then(res => res.json())
+      // .then(newFavoriteInDB)
+      }
+    else{
+      window.alert("Give this location a name!")
+    }
+    map.setCenter({lat:newFavorite.latitude, lng: newFavorite.longitude})
+
+  }
+
 
 })
 
 
-document.querySelector("form").addEventListener("submit", e =>{
-  e.preventDefault();
-  const x = parseFloat(e.target.latitude.value);
-  const y = parseFloat(e.target.longitude.value);
-  const location = e.target.location.value;
-  if(e.target["add-to-list"].checked){
-    const newFavorite = {
-      location: location,
-      latitude: x,
-      longitude: y
-    }
-    fetch("http://localhost:3000/favorites",{
-      method:"POST",
-      headers:{
-        //sending JSON to db
-        "Content-Type": "applications/json",
-        //expecting a copy of the JSON back as confirmation
-        "Accept": "applications/json" 
-    },
-    body: JSON.stringify(newFavorite)
-    })
-    .then(res => res.json())
-    .then(newFavoriteInDB)
-  }
-  if(!isNaN(x) && x <= 90 && x>=-90 && y <= 180 && y>=-180 &&!isNaN(y)){
-    const newPosition = {lat:x, lng:y}
-    const zoom = 11
-    map.zoom = zoom
-    map.setCenter(newPosition)
-  } else{
-    console.error("Woof invalid coordinates")
-  }
-  });
+clicked = null;
 
-  // const inputs = document.querySelectorAll('input[type="text"]')
+///////////////////////////////////////////////////////////////////
 
-  // inputs.forEach(input =>{
-  //   input.addEventListener("focus", e =>{
-  //     e.preventDefault();
-  //     e.target.style.background = "yellow"
-  //   })
-  // })
-
-  // inputs.forEach(input =>{
-  //   input.addEventListener("focusout", e =>{
-  //     e.preventDefault();
-  //     e.target.style.background = "white"
-  //   })
-  // })
 
 
 async function success(position){
@@ -121,19 +171,34 @@ document.querySelector("#random").addEventListener("click", e =>{
 
 })
 
-const favorites = document.querySelector("#favorites")
-fetch("http://localhost:3000/favorites")
-.then(res => res.json())
-.then(resConverted =>{
-  resConverted.forEach(favorite =>{
+async function displayFavorites(){
+  const response = await fetch("http://localhost:3000/favorites")
+  const convertedResponse = await response.json()
+  const favorites = document.querySelector("#favorites")
+
+  convertedResponse.forEach(favorite =>{
     const liElement = document.createElement("li")
     liElement.textContent = favorite.location
     liElement.latitude = favorite.latitude
     liElement.longitude = favorite.longitude
     favorites.append(liElement)
-    }
-  )}
-);
+
+  })
+}
+
+displayFavorites()
+// fetch("http://localhost:3000/favorites")
+// .then(res => res.json())
+// .then(resConverted =>{
+//   resConverted.forEach(favorite =>{
+//     const liElement = document.createElement("li")
+//     liElement.textContent = favorite.location
+//     liElement.latitude = favorite.latitude
+//     liElement.longitude = favorite.longitude
+//     favorites.append(liElement)
+//     }
+//   )}
+// );
 
 document.querySelector("#favorites").addEventListener("click", e =>{
   e.preventDefault();
@@ -146,3 +211,37 @@ document.querySelector("#favorites").addEventListener("click", e =>{
   map.setCenter(position);
 }
 )
+
+async function displayLastLocation(){
+  const response = await fetch("http://localhost:3000/favorites")
+  const convertedResponse = await response.json()
+  const lastLocation = convertedResponse.pop()
+  try{
+    map.setCenter({lat:lastLocation.latitude, lng:lastLocation.longitude})
+  } catch{
+    console.log("*bandaid* looking to call last favorites to simulate refresh for favorites and no refresh for map")
+  }
+}
+
+displayLastLocation()
+
+
+
+//////Logic for extra event listener
+
+
+  // const inputs = document.querySelectorAll('input[type="text"]')
+
+  // inputs.forEach(input =>{
+  //   input.addEventListener("focus", e =>{
+  //     e.preventDefault();
+  //     e.target.style.background = "yellow"
+  //   })
+  // })
+
+  // inputs.forEach(input =>{
+  //   input.addEventListener("focusout", e =>{
+  //     e.preventDefault();
+  //     e.target.style.background = "white"
+  //   })
+  // })
